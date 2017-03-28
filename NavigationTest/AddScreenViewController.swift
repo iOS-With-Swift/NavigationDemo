9dc77e95fb31dc5f7e6926df0f8afa99a958ea0f
PhongLe <phongle@PhongLes-MacBook-Pro.local>
2017-03-28 20:08:13 +0700

    final commit

--- NavigationTest/AddScreenViewController.swift
+++ NavigationTest/AddScreenViewController.swift
@@ -6,4 +6,26 @@
 //  Copyright © 2017 PhongLe. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class AddScreenViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func cancelBtn(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    @IBAction func saveBtn(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
